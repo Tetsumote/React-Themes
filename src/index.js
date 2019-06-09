b6f8b3051a301a7daf43e618e924ef5f32f7c78f
Paweł Grajewski <unionproject88@gmail.com>
2019-06-09 19:46:00 +0200

    Complite example
    Easy to start and play with react themes.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import theme from "styled-theming";
 import styled, { ThemeProvider } from "styled-components";
+import PropTypes from "prop-types";
 
 import "./styles.css";
 
@@ -15,15 +16,42 @@ const textColor = theme("mode", {
   dark: "#fff"
 });
 
+const padding = theme("layout", {
+  compact: "0.5rem",
+  cozy: "1.5rem"
+});
+
+const buttonFontSize = theme.variants("layout", "kind", {
+  small: { compact: "0.9rem", cozy: "1.2rem" },
+  medium: { compact: "1rem", cozy: "1.3rem" },
+  large: { compact: "2rem", cozy: "1.4rem" }
+});
+
+const Button = styled.button`
+  font-size: ${buttonFontSize};
+`;
+
+Button.propTypes = {
+  kind: PropTypes.oneOf(["small", "medium", "large"])
+};
+
+Button.defaultProps = {
+  kind: "large"
+};
+
 const Wrapper = styled.div`
   background-color: ${backgroundColor};
   color: ${textColor};
+  padding: ${padding};
 `;
 
 export default function App() {
   return (
-    <ThemeProvider theme={{ mode: "light" }}>
-      <Wrapper>Hello World</Wrapper>
+    <ThemeProvider theme={{ mode: "dark", layout: "compact" }}>
+      <Wrapper>
+        Welcome
+        <Button>Sign in</Button>
+      </Wrapper>
     </ThemeProvider>
   );
 }
